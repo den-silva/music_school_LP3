dd337a3e11b0879da88bf3520b3ffbab31515d5c
AldaniloSilva <aldanilosilva@gmail.com>
2020-10-31 18:18:21 -0300

    Adicionando implementacao das classes da entidade Professores

---
+++ src/ftt/dao/ProfessoresDao.java
@@ -0,0 +1,40 @@
+package ftt.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import ftt.model.Professores;
+
+public class ProfessoresDao implements IcrudPadrao<Professores>{
+
+	@Override
+	public void insert(Professores entidade) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Professores entidade) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Professores entidade) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Professores findForId(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Professores> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ftt/model/Professores.java
@@ -0,0 +1,64 @@
+package ftt.model;
+
+public class Professores {
+	
+	private int id_professor;
+	private String nome;
+	private Endereco endereco;
+	private String email;
+	private String senha;
+	
+	public Professores () {
+		
+	}
+
+	public int getId_professor() {
+		return id_professor;
+	}
+
+	public void setId_professor(int id_professor) {
+		this.id_professor = id_professor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+
+}
